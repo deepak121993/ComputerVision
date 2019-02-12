5cf05a67eba1ca40740470953b9aab2f00d10744
deepak121993 <40932231+deepak121993@users.noreply.github.com>
2019-02-13 01:13:25 +0530

    bug

--- pyImageSearch/preprocessing/imagetoarrayprocessor.py
+++ pyImageSearch/preprocessing/imagetoarrayprocessor.py
@@ -9,5 +9,5 @@ class ImageToArrayProcessor:
     def preprocess(self,image):
 
 
-        return img_to_array(image,dataFormat=self.dataFormat)
+        return img_to_array(image,data_format=self.dataFormat)
         
