48be3bfa7c3001a32e05ef8df373c1a4c7bb6b07
deepak121993 <deepaksaini121993@gmail.com>
2019-02-14 00:44:36 +0530

    bug

--- pyImageSearch/nn/conv/mini_vgg_net.py
+++ pyImageSearch/nn/conv/mini_vgg_net.py
@@ -1,9 +1,12 @@
-from keras.layers.convolutional import Conv2D
-from keras.layers.core import Activation,Flatten,Dense
-from keras.layers import MaxPooling2D
-from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
-from keras import backend as K 
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
 
 
 class MiniVGGNet:
