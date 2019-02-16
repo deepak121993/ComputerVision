e19041e6561dc2c4ecbc42232a04de5cfb404a46
deepak121993 <deepaksaini121993@gmail.com>
2019-02-16 10:18:49 +0530

    over/under fitting

---
+++ cifar_10_monitor.py
@@ -0,0 +1,53 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from pyimagesearch.callbacks.trainingmonitor import TrainingMonitor
+from sklearn.preprocessing import LabelBinarizer
+from pyimagesearch.nn.conv.minivggnet import MiniVGGNet
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+import argparse
+import os
+
+
+
+ap = ArgumentParser()
+ap.add_argument("-o","--output",required=True)
+args = vars(ap.parse_args())
+
+print("[INFO] Process ID :{}".format(os.getpid()))
+
+
+
+print("[INFO] loading CIFAR-10 data...")
+
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+
+trainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+labelNames = ["airplane", "automobile", "birds", "cat", "deer", "dog", "frog",
+     "horse", "ship", "truck"]
+
+print("[INFO] compiling model")
+opt = SGD(lr=0.01, decay=0.01/40, momentum=0.9, nesterov=True)
+
+model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+    metrics=["accuracy"])
+
+##construct the set of callbacks
+
+figPath = os.path.sep.join([args["output"],"{}.png".format(os.getpid())])
+jsonPath = os.path.sep.join([args["output"],"{}.json".format(os.getpid())])
+callbacks=[TrainingMonitor(figPath,jsonPath=jsonPath)]
+print("[INFO] training the network...")
+
+
+model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=64,
+    epochs=50, verbose=1,callbacks=callbacks)
+
---
+++ pyImageSearch/callbacks/trainingmonitor.py
@@ -0,0 +1,69 @@
+
+from keras.callbacks import BaseLogger
+import matplotlib.pyplot as plt 
+import numpy as np 
+import json
+import os
+
+#base logger is logs the accuracy and loss in disk
+
+class TrainingMonitor(BaseLogger):
+
+    def __init__(self,figPath,jsonPath=None,startAt=0):
+
+        super(TrainingMonitor,self).__init__()
+        self.figPath = figPath
+        self.jsonPath = jsonPath
+        self.startAt = startAt
+
+    def on_train_begin(self,logs=[]):
+        self.H = {}
+
+        ##if the jsonPath exists ::
+        if self.jsonPath is not None:
+            if os.path.exists(self.jsonPath):
+
+                self.H=json.loads(open(self.jsonPath).read())
+
+                ##check se if starting epochs are provided :
+
+                if self.startAt >0:
+
+                    for k in self.H.keys:
+                        self.H[k] = self.H[k][:self.startAt]
+
+
+    def on_epoch_end(self,epochs,logs={}):
+        ##loop over the logs and update the accuracy and logs for each epochs
+
+
+        for (k,v) in logs.items():
+            l = self.h.get(k,[])
+            l.append(v)
+            self.H[k] =l
+
+        if self.H.jsonPath is not None:
+            f = open(self.jsonPath,"w")
+            f.write(json.dumps(self.H))
+            f.close()
+
+
+        if len(self.H["loss"]) > 1:
+            # plot the training loss and accuracy
+            N = np.arange(0, len(self.H["loss"]))
+            plt.style.use("ggplot")
+            plt.figure()
+            plt.plot(N, self.H["loss"],  label="train_loss")
+            plt.plot(N, self.H["val_loss"],  label="val_loss")
+            plt.plot(N, self.H["acc"],  label="train_acc")
+            plt.plot(N, self.H["val_acc"],  label="val_acc")
+            plt.title("Training Loss and Accuracy [Epoch {}]".format(
+                len(self.H["loss"])))
+            plt.xlabel("Epoch #")
+            plt.xlabel("Loss/Accuracy")
+            plt.legend()
+
+            # save the figure
+            plt.savefig(self.figPath)
+            plt.close()
+
