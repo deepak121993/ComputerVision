ea8d0307e0d6713199fa4a556641ec7927b98ea5
deepak121993 <deepaksaini121993@gmail.com>
2019-02-24 17:44:53 +0530

    fine-tuning

--- caltech_finetuning.py
+++ caltech_finetuning.py
@@ -83,7 +83,7 @@ opt = SGD(lr=0.001)
 model.compile(loss="categorical_crossentropy",optimizer=opt,metrics=["accuracy"])
 
 print("[INFO] fine tunining model ...")
-model.fit_generator(aug.flow(trainX,trainY,batch_size=32),validation_data=(testX,testY),epochs=10\
+model.fit_generator(aug.flow(trainX,trainY,batch_size=32),validation_data=(testX,testY),epochs=10,\
 steps_per_epoch=len(trainX)//32,verbose=1)
 
 print("[INFO] evaluating after fine-tuning..")
